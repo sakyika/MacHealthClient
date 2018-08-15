12dfea7aa1339233ede0ed459e1f2c19be8fc518
Kwadwo Sakyi <kwadwosakyi@gmail.com>
2018-08-15 12:17:13 -0400

    redux and redux form added

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
+import Signin from './Signin';
+
+const Dashboard = () => <h2>Dashboard</h2>
+const Signout = () => <h2>Signout</h2>
+const Landing = () => <h2>Landing</h2>
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to McMaster Health Sciences!</h1>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route path="/" exact component={ Landing } />
+            <Route path="/signin" component={ Signin } />
+            <Route path="/signout" component={ Signout } />
+            <Route path="/dashboard" component={ Dashboard } />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import App from './components/App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducers';
+
+const store = createStore( reducers, {}, applyMiddleware());
+
+
+ReactDOM.render(
+    <Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
